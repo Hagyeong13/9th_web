6f3528c7395601e356c80b4a8572cae00cda02ae
Eunjin Oh <ibsuny3214@naver.com>
2025-09-29 02:07:03 +0900

    Feat : useEffect 활용하여 영화 데이터 불러오기

---
+++ week03/mission/mission01/src/pages/moviePages.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import type { Movie, Movies } from "../types/movies"
+import axios from "axios";
+
+const MoviePage = () => {
+    //1. api에서 가져온 데이터 저장할 useState 만들기
+    const [movies,setMovies] = useState<Movie[]>([]);
+
+    console.log(movies);
+
+    //2. useEffect 사용해서 api로 값 저장하기
+    useEffect (()=> {
+        const loadPopularMovie = async () => {
+            const { data } = await axios.get<Movies>(
+                'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
+                {
+                    headers: {
+                        Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
+                    },
+                }
+            )
+
+            setMovies(data.results);
+        }
+
+        loadPopularMovie();
+    },[]);
+
+
+     return (
+        <ul className="container mx-auto px-16 py-4 grid grid-cols-6 gap-2">
+            {movies.map((m) => (
+                <li key={m.id}>
+                    <img src={`https://image.tmdb.org/t/p/w500${m.poster_path}`} className="w-1.9 rounded-md hover:blur"/>
+                </li> 
+            ))}
+        </ul>
+    );
+}
+
+export default MoviePage;
