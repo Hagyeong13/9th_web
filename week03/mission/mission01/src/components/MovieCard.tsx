4b971ba87b40ae69ef05590aaaa0ea08080c5822
Eunjin Oh <ibsuny3214@naver.com>
2025-09-29 02:56:38 +0900

    Feat : Hover 효과 구현

--- week03/mission/mission01/src/components/MovieCard.tsx
+++ week03/mission/mission01/src/components/MovieCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { Movie } from "../types/movies";
 
 interface MovieCardProps {
@@ -5,12 +6,19 @@ interface MovieCardProps {
 }
 
 const MovieCard = ({movie} : MovieCardProps) => {
+    const [isHover, setIshover] = useState(false);
 
     return (
         <>
-            <li key={movie.id}>
-                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} className="w-1.9 rounded-md hover:blur"/>
-            </li> 
+            <li className="relative group cursor-pointer rounded-xl shadow-lg w-44 transition-transform duration-500 hover:scale-105" key={movie.id} onMouseEnter={():void => setIshover(true)} onMouseLeave={():void=>setIshover(false)}>
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} className="rounded-xl"/>
+                {isHover && (
+                    <div className="absolute inset-0 rounded-xl bg-gradient-to-t from-black/50 to-transparent backdrop-blur-md flex flex-col items-center justify-center p-4">
+                        <h2 className='text-lg font-bold text-center mt-1 text-white'>{movie.title}</h2>
+                        <p className='text-xs mt-1 line-clamp-5 text-gray-300'>{movie.overview}</p>
+                    </div>
+                )} 
+            </li>
         </>
     )
 }
