c0ba3c86f519beca3785b9340843aa8d413165ce
Eunjin Oh <ibsuny3214@naver.com>
2025-09-29 04:17:33 +0900

    Feat : 로딩 생성

--- week03/mission/mission02/src/pages/MoviePage.tsx
+++ week03/mission/mission02/src/pages/MoviePage.tsx
@@ -5,38 +5,50 @@ import axios from "axios";
 import MovieCard from "../components/MovieCard";
 
 const MoviePage = () => {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const location = useLocation();
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const location = useLocation();
+    const [isLoading,setIsloading] =useState(true);
 
-  useEffect(() => {
-    const loadMovies = async () => {
-      let endpoint = "popular";
-      if (location.pathname === "/playing") endpoint = "now_playing";
-      else if (location.pathname === "/top_rated") endpoint = "top_rated";
-      else if (location.pathname === "/upcoming") endpoint = "upcoming";
+    useEffect(() => {
+        const loadMovies = async () => {
+            let endpoint = "popular";
+            if (location.pathname === "/playing") endpoint = "now_playing";
+            else if (location.pathname === "/top_rated") endpoint = "top_rated";
+            else if (location.pathname === "/upcoming") endpoint = "upcoming";
 
-      const { data } = await axios.get<Movies>(
-        `https://api.themoviedb.org/3/movie/${endpoint}?language=en-US&page=1`,
-        {
-          headers: {
-            Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
-          },
-        }
-      );
+            setIsloading(true);
+            const { data } = await axios.get<Movies>(
+                `https://api.themoviedb.org/3/movie/${endpoint}?language=en-US&page=1`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
+                    },
+                }
+            );
 
-      setMovies(data.results);
-    };
+            setMovies(data.results);
+            setIsloading(false);
+        };
 
-    loadMovies();
-  }, [location.pathname]);
+        loadMovies();
+    }, [location.pathname]);
 
-  return (
-    <ul className="container mx-auto px-16 py-4 grid grid-cols-6 gap-2">
-      {movies.map((m) => (
-        <MovieCard key={m.id} movie={m} />
-      ))}
-    </ul>
-  );
+    return (
+        <>
+            {!isLoading && (
+                <ul className="container mx-auto px-16 py-4 grid grid-cols-6 gap-2">
+                    {movies.map((m) => (
+                        <MovieCard key={m.id} movie={m} />
+                    ))}
+                </ul>
+            )}
+            {isLoading && (
+                <div className="flex justify-center items-center h-screen">
+                    <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-black-500"></div>
+                </div>
+            )}
+        </>
+    );
 };
 
 export default MoviePage;
