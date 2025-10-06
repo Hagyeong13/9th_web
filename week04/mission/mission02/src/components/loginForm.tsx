03c186bce25606f1e926ab8840adb60da6d90449
hagyeong <hagyeong13@naver.com>
2025-10-07 03:27:58 +0900

    Feat : 로그인 ui 구축

--- week04/mission/mission02/src/components/loginForm.tsx
+++ week04/mission/mission02/src/components/loginForm.tsx
@@ -5,7 +5,7 @@ interface LoginFormProps {
 const LoginForm = ({context} : LoginFormProps) => {
     return (
         <>
-            <form className="flex w-70 h-10 rounded text-gray-400 border border-color-gray items-center justify-start px-3 mb-6">{context}</form>
+            <form className="flex w-70 h-10 rounded text-gray-400 border border-color-gray items-center justify-start px-3 mb-3 text-sm">{context}</form>
         </>
     )
 }
--- week04/mission/mission02/src/pages/loginPage.tsx
+++ week04/mission/mission02/src/pages/loginPage.tsx
@@ -2,9 +2,12 @@ import LoginForm from "../components/loginForm";
 
 const LoginPage = () => {
     return (
-        <>
-            <LoginForm context="text"/>
-        </>
+        <div className="flex flex-col justify-center items-center h-screen -mt-10">
+            <h1 className="flex text-3xl font-semibold mb-6">[로그인]</h1>
+            <LoginForm context="아이디를 입력하세요."/>
+            <LoginForm context="비밀번호를 입력하세요."/>
+            <button className="flex w-70 h-10 rounded bg-gray-400 items-center justify-center mb-3 text-sm font-semibold">로그인</button>
+        </div>
     )
 }
 
