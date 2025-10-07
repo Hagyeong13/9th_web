0688c8a6ff8df639384821ed99045ec571674e7f
hagyeong <hagyeong13@naver.com>
2025-10-08 00:16:00 +0900

    Feat: useForm을 이용한 사용자 입력 유효성 검사 & 에러 메시지 표시

--- week04/mission/mission02/src/pages/loginPage.tsx
+++ week04/mission/mission02/src/pages/loginPage.tsx
@@ -1,5 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import LoginForm from "../components/loginForm";
+import useForm from "../hooks/useForm";
+import type { UserSignInformation } from "../utils/validate";
+import { validateSignin } from "../utils/validate";
 
 const LoginPage = () => {
     const navigate = useNavigate();
@@ -9,6 +12,18 @@ const LoginPage = () => {
         else navigate("/");
     };
 
+    const form = useForm<UserSignInformation>({
+        initialValue: { email: "", password: "" },
+        validate: validateSignin,
+    });
+    const { values, errors } = form;
+
+    const handleSubmit = () => {
+    };
+
+    const isDisabled =
+        Object.values(errors||{}).some((error)=>error.length>0) ||
+        Object.values(values).some((value)=>value==="");
     return (
         <>
             <div className="flex flex-col justify-center items-center h-screen -mt-15">
@@ -16,9 +31,11 @@ const LoginPage = () => {
                     <button onClick={handleBack} className="text-3xl font-bold absolute left-0 ml-8 cursor-pointer">&lt;</button>
                     <h1 className="text-3xl font-semibold text-center w-full">[로그인]</h1>
                 </div>
-                <LoginForm context="아이디를 입력하세요."/>
-                <LoginForm context="비밀번호를 입력하세요."/>
-                <button className="flex w-70 h-10 rounded bg-gray-400 items-center justify-center mb-3 text-sm font-semibold cursor-pointer">로그인</button>
+                <LoginForm name="email" context="아이디를 입력하세요."  {...form} />
+                <LoginForm name="password" context="비밀번호를 입력하세요."  {...form} />
+                <button onClick={handleSubmit} disabled={isDisabled}
+                    className={`flex w-70 h-10 rounded items-center justify-center mb-3 text-sm font-semibold
+                        ${isDisabled ? "cursor-not-allowed bg-white border border-gray-300 text-gray-300" :"cursor-pointer bg-gray-200 border border-gray-300" }`}>로그인</button>
             </div>
         </>
     )
